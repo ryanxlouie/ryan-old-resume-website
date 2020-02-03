4f4b49d85a89c9f445232d364dd03afb21621af4
Ryan Louie <yaylouie@gmail.com>
2020-02-02 20:50:08 -0800

    Updates

---
+++ precache-manifest.09c8f7aa4958fb06ff6f63e1b783124e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27a2933f932d9f3060a8961c2a86813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd101d05dd80f8b51d9",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4f01fbf5a58d142c7e77",
+    "url": "/static/css/main.642f8b80.chunk.css"
+  },
+  {
+    "revision": "7dd101d05dd80f8b51d9",
+    "url": "/static/js/2.f86f2f08.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.f86f2f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f01fbf5a58d142c7e77",
+    "url": "/static/js/main.97644e64.chunk.js"
+  },
+  {
+    "revision": "f1380d0127b6b6a6e374",
+    "url": "/static/js/runtime-main.76792e8e.js"
+  },
+  {
+    "revision": "23f74061b8ae85e26e49392790b7f124",
+    "url": "/static/media/ExpressIcon.23f74061.png"
+  },
+  {
+    "revision": "22716d5c5e038f0c54ace100c61d383a",
+    "url": "/static/media/MountFuji.22716d5c.jpg"
+  },
+  {
+    "revision": "eb663c567edf36758e167eba9853d097",
+    "url": "/static/media/ProfilePic2.eb663c56.jpg"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/static/media/PythonIcon.067050f6.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/ReactIcon.47ce6e77.png"
+  },
+  {
+    "revision": "d8350607125b65bb75b4464a47511217",
+    "url": "/static/media/VolleyballIcon.d8350607.png"
+  },
+  {
+    "revision": "cac86d7b0dd25823cccc91a9b503d39b",
+    "url": "/static/media/XboxIcon.cac86d7b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5143cd974b4e327521785e43aac62a65.js"
+  "/precache-manifest.09c8f7aa4958fb06ff6f63e1b783124e.js"
 );
 
 self.addEventListener('message', (event) => {
